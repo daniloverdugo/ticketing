33ea20d1cb54d11171df9f92b146abc51e659d35
danilousach <danilo.verdugo@usach.cl>
2022-09-02 19:34:44 -0400

    modified:   auth/src/index.ts

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -3,7 +3,7 @@ import mongoose from 'mongoose';
 import { app } from './app';
 
 const start = async () => {
-  console.log('iniciando...')
+  console.log('iniciando..')
   if (!process.env.JWT_KEY) {
     throw new Error('JWT_KEY must be defined');
   }
